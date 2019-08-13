4859a029e7e9910198057e2868a59d0992e9cdb5
Rett Berg <rett@google.com>
2019-08-13 08:49:21 -0700

    add initial wakestore code

--- wakeStore/__init__.py
+++ wakeStore/__init__.py
@@ -1,23 +1,5 @@
-# ⏾🌊🛠 wake software's true potential
-#
-# Copyright (C) 2019 Rett Berg <github.com/vitiral>
-#
-# The source code is Licensed under either of
-#
-# * Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
-#   http://www.apache.org/licenses/LICENSE-2.0)
-# * MIT license ([LICENSE-MIT](LICENSE-MIT) or
-#   http://opensource.org/licenses/MIT)
-#
-# at your option.
-#
-# Unless you explicitly state otherwise, any contribution intentionally submitted
-# for inclusion in the work by you, as defined in the Apache-2.0 license, shall
-# be dual licensed as above, without any additional terms or conditions.
-
-from wake.utils import *
+from .utils import *
 import jshlib
-
 
 class Store(object):
     """
@@ -120,3 +102,7 @@ def load_pkg_meta(pcache_path):
 
 def pkg_meta_path(pcache_path):
     return path.join(pcache_path, DIR_WAKE, FILE_STORE_META)
+
+
+def main(argv):
+    pass
